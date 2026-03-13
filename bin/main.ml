cc269000f03725ea7f8148890cbd2f2c9f948734
Olivier Nicole <olivier@chnik.fr>
2026-03-13 16:27:36 +0100

    Fix for Cmdliner >=2.0.0

--- bin/main.ml
+++ bin/main.ml
@@ -78,15 +78,15 @@ let period =
   Term.(const f $ from $ to_ $ last_week)
 
 let user : User.t Term.t =
-  let str_parser, str_printer = Arg.string in
+  let str_parser, str_printer = Arg.(Conv.parser string, Conv.pp string) in
   let parser x =
-    match str_parser x with `Ok x -> `Ok (User.User x) | `Error e -> `Error e
+    match str_parser x with Ok x -> Ok (User.User x) | Error e -> Error e
   in
   let printer fs = function
     | User.Viewer -> str_printer fs "viewer"
     | User x -> str_printer fs x
   in
-  let user_conv = (parser, printer) in
+  let user_conv = Arg.Conv.make ~parser ~pp:printer ~docv:"A string" () in
   let doc = Arg.info ~doc:"User name" [ "user" ] in
   Arg.(value & opt user_conv Viewer & doc)
 
